4e4f7a0f61b21ab429f6953329c8b58f8facf7d5
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-27 01:11:34 +0330

    feat: complete Controller class methods and properties

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,5 +4,27 @@ namespace core;
 
 class Controller
 {
+{
+    private string $layout = 'main';
+    public function render($view, $params = [])
+    {
+        return Application::getApp()->getRouter()->renderView($view, $params);
+    }
+
+    /**
+     * @return string
+     */
+    public function getLayout(): string
+    {
+        return $this->layout;
+    }
+
+    /**
+     * @param string $layout
+     */
+    public function setLayout(string $layout): void
+    {
+        $this->layout = $layout;
+    }
 
 }
