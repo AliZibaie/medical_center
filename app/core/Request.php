5079df461d9ba08737af14e14997a1ab186bf4b7
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-27 01:14:25 +0330

    feat: complete Request class methods and properties

--- app/core/Request.php
+++ app/core/Request.php
@@ -4,4 +4,41 @@ namespace core;
 
 class Request
 {
+    function getPath()
+    {
+        $path =  $_SERVER['REQUEST_URI'] ?? '/';
+        $position = strpos($path,'?');
+        if (!$position){
+            return $path;
+        }
+        return substr($path, 0, $position);
+    }
+    function method()
+    {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+    function isGet()
+    {
+        return $this->method() === 'get';
+    }
+    function isPost()
+    {
+        return $this->method() === 'post';
+    }
+
+    public function getBody()
+    {
+        $body = [];
+        if ($this->method() === 'get'){
+            foreach ($_GET as $key => $value){
+                $body[$key] = filter_input(INPUT_GET, $key , FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+        if ($this->method() === 'post'){
+            foreach ($_POST as $key => $value){
+                $body[$key] = filter_input(INPUT_POST, $key , FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+        return $body;
+    }
 }
