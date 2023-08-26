8055b3f59c18c5c3713258f0e698efff15d4d63c
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-26 19:00:21 +0330

    feat: add setStatusCode method in Response class

--- app/core/Response.php
+++ app/core/Response.php
@@ -1,8 +1,11 @@
 <?php
 
-namespace core;
+namespace app\core\Response;
 
 class Response
 {
-
+    public function setStatusCode(int $code) : void
+    {
+        http_response_code($code);
+    }
 }
