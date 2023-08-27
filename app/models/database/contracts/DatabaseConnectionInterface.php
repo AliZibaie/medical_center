644aa86efca7d578c88b1a2c9c8700b5267b1244
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-27 20:22:17 +0330

    style: add space

--- app/models/database/contracts/DatabaseConnectionInterface.php
+++ app/models/database/contracts/DatabaseConnectionInterface.php
@@ -1,5 +1,7 @@
 <?php
 namespace app\app\models\database\contracts;
+use PDO;
+
 interface DatabaseConnectionInterface {
     public static function getInstance();
     public function getConnection() : PDO;
