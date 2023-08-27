b42c27ab291679e54d933076bb4c6dc0df70cc23
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-27 14:37:06 +0330

    feat: add connection method

--- app/models/database/MySqlDatabaseConnection.php
+++ app/models/database/MySqlDatabaseConnection.php
@@ -1,12 +1,32 @@
 <?php
 namespace app\app\models\database;
+USE PDO;
 class MySqlDatabaseConnection implements DatabaseConnectionInterface {
 
     private static $obj;
-    private  $pdo;
-    private final function __construct()
+    private PDO $pdo;
+    private array $config;
+    private function __construct()
     {
-      $this->pdo = new PDO("mysql:host=localhost;dbname=mydb", 'root', "");
+        $this->config = include(__DIR__ . '/../../config/database.php');
+        $this->pdo = $this->connect();
+    }
+
+    public function connect()
+    {
+        $driver = $this->config['driver'];
+        $dbname = $this->config['dbname'];
+        $host = $this->config['host'];
+        $username = $this->config['username'];
+        $password = $this->config['password'];
+
+        $dsn = "$driver:host=$host;dbname=$dbname";
+
+        $pdo = new PDO($dsn, $username, $password);
+
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+        return $pdo;
     }
     public function getConnection():PDO
     {
