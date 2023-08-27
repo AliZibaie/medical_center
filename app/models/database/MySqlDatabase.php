7e692a7c7293e43b867895878d09df8a8c983bca
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-27 14:44:11 +0330

    feat: add MySqlDatabase  methods and namespaces

--- app/models/database/MySqlDatabase.php
+++ app/models/database/MySqlDatabase.php
@@ -1,57 +1,21 @@
 <?php
+
+use app\app\models\database\contracts\DatabaseInterface;
+use app\app\models\database\MySqlDatabaseConnection;
+
+
 class MySqlDatabase implements DatabaseInterface {
 
-private $connection;
+private MySqlDatabaseConnection $instance;
+private PDO $connection;
 private $sql;
 
-public function __construct(MySqlDatabaseConnection $connection) {
-$this->connection = $connection->getConnection();
+public function __construct(MySqlDatabaseConnection|\app\app\models\database\contracts\MySqlDatabaseConnection $instance) {
+$this->instance = $instance;
+$this->connection = $instance->getConnection();
 return $this->connection;
 }
 
-//public function table(string $table) : DatabaseInterface {
-//$this->query = 'SELECT * FROM ' . $table;
-//return $this;
-//}
-//
-//public function select(array $cols=['*']) : DatabaseInterface {
-//
-//return $this;
-//}
-//
-//public function insert(array $fields) : DatabaseInterface {
-//$this->query = 'INSERT INTO ' . $table . ' (' . implode(', ', array_keys($fields)) . ') VALUES (' . implode(', ', array_values($fields)) . ')';
-//return $this;
-//}
-//
-//public function update(array $fields) : DatabaseInterface {
-//$this->query = 'UPDATE ' . $table . ' SET ' . implode(', ', array_map(function ($k, $v) {
-//return $k . ' = ' . $v;
-//}, array_keys($fields), array_values($fields)));
-//return $this;
-//}
-//
-//public function where(string $val1, string $val2, string $operation = '=') : DatabaseInterface {
-//$this->query .= ' ' . $operation . ' ' . $val2;
-//return $this;
-//}
-
-//public function fetch() {
-//$stmt = $this->connection->prepare($this->query);
-//$stmt->execute();
-//return $stmt->fetchAll();
-//}
-//
-//public function fetchAll() {
-//$stmt = $this->connection->prepare($this->query);
-//$stmt->execute();
-//return $stmt->fetchAll(PDO::FETCH_ASSOC);
-//}
-//
-//public function exec() : bool {
-//$stmt = $this->connection->prepare($this->query);
-//return $stmt->execute();
-//}
     public function table(string $table) : DatabaseInterface
     {
         $this->sql = ' ' . $table;
