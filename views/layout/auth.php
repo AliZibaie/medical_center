8cfda80ed19e409db81c05cde9ebd84346edb75a
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-28 02:45:11 +0330

    feat: add different layout for authentication forms

--- views/layout/auth.php
+++ views/layout/auth.php
@@ -1,1 +1,15 @@
-<?php
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./dist/output.css" rel="stylesheet">
+    <title>{{title}}</title>
+<!--    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.6.2/dist/full.css" rel="stylesheet" type="text/css" />-->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<main class="mb-12 mt-8"><div class="mx-auto">{{content}}</div></main>
+<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</body>
+</html>
