f406f0046bf1a9137be81beb24a69fd58e248ffb
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-28 01:13:03 +0330

    feat: add accept

--- app/controllers/ManagementStatusSiteController.php
+++ app/controllers/ManagementStatusSiteController.php
@@ -7,9 +7,41 @@ use app\app\core\SiteController;
 
 class ManagementStatusSiteController extends SiteController
 {
-    public function accept() : void
+
+    public function show(): string
     {
-        $newDoctor = $_POST["accept"];
-        Application::getApp()->getDatabase()->update(["confirmation_status"=>1])->table('doctor');
+
+            $params = Application::getApp()->getDatabase()->table('doctor')->where('confirmation_status', val2: '0')->select()->fetchAll();
+            $params = [$params, "job" => 'doctor'];
+            unset($_POST);
+            return $this->render('managementStatus', $params);
     }
+
+    public function control()
+    {
+
+        if (isset($_POST['accept']) && Application::getApp()->getRequest()->getQueryParameter() == 'doctor') {
+            $id = $_POST['accept'];
+            Application::getApp()->getDatabase()->table('doctor')->update(["confirmation_status"=>1])->where('id',val2:  $id)->exec();
+            return $this->show();
+        }
+        if (isset($_POST['accept']) && array_keys($_POST)[1] == 'doctor' && isset($_POST['doctor'])) {
+            $id = $_POST['id'];
+            Application::getApp()->getDatabase()->table('doctor')->update(["confirmation_status"=>1])->where('id',val2:  $id)->exec();
+            return $this->show();
+        }
+
+        if (Application::getApp()->getRequest()->getQueryParameter() == 'doctor'){
+            return $this->show();
+        }
+        if (Application::getApp()->getRequest()->getQueryParameter() == 'manager'){
+            $params = Application::getApp()->getDatabase()->table('manager')->where('confirmation_status', val2: '0')->select()->fetchAll();
+            $params = [$params, "job" => 'manager'];
+            return $this->render('managementStatus', $params);
+        }
+        return $this->show();
+    }
+
+
+
 }
