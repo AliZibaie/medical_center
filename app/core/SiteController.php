9edcd7a4e3ed92d2a9613322bc7b7eb0f51706c8
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-27 20:17:48 +0330

    chore: rename file

---
+++ app/core/SiteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\app\core;
+USE app\app\core\Application;
+class SiteController
+{
+
+    private string $layout = 'main';
+    public function render($view, $params = [])
+    {
+        return Application::getApp()->getRouter()->renderView($view, $params);
+    }
+
+    /**
+     * @return string
+     */
+    public function getLayout(): string
+    {
+        return $this->layout;
+    }
+
+    /**
+     * @param string $layout
+     */
+    public function setLayout(string $layout): void
+    {
+        $this->layout = $layout;
+    }
+
+}
