3295aa047dac6a030cb881d2b67e19d466eb9e8a
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-28 02:43:54 +0330

    feat: add signup  template

--- views/signup.php
+++ views/signup.php
@@ -1,62 +1,68 @@
-<div
-    class="block max-w-sm rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-    <form>
-        <!--E-mail input-->
-        <div class="relative mb-12" data-te-input-wrapper-init>
-            <input
-                type="email"
-                class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Enter email" />
-            <label
-                for="exampleInputEmail1"
-                class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-            >Email address</label
-            >
-            <small
-                id="emailHelp"
-                class="absolute w-full text-neutral-500 dark:text-neutral-200"
-                data-te-input-helper-ref
-            >We'll never share your email with anyone else.</small
-            >
+<form method="post"  id="signupform">
+    <div class="container mx-auto my-20 space-y-5 flex flex-col justify-between px-5 py-8 rounded-2xl bg-wood hover:bg-wood2 hover:shadow-2xl border-2 border-black" style="width: 500px;">
+        <!-- header title -->
+        <div class="header flex justify-center text-3xl text-black">Register Form</div>
+
+        <!-- full name -->
+        <div class="input-group flex flex-col space-y-5">
+            <label for="username" class="text-2xl text-black">Username</label>
+            <input type="text" name="username" id="username" class="border-2 border-gray-500 p-3 rounded-md text-lg focus:text-green-700 focus:outline-blue-700" placeholder="Enter your username">
         </div>
-as
-        <!--Password input-->
-        <div class="relative mb-6" data-te-input-wrapper-init>
-            <input
-                type="password"
-                class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                id="exampleInputPassword1"
-                placeholder="Password" />
-            <label
-                for="exampleInputPassword1"
-                class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-            >Password</label
-            >
+        <!-- password -->
+        <div class="input-group flex flex-col space-y-5">
+            <label for="password" class="text-2xl text-black ">Password</label>
+            <input type="password" name="password" id="password" class="border-2 border-gray-500 p-3 rounded-md text-lg focus:text-green-700 focus:outline-blue-700" placeholder="Enter your password">
+        </div>
+        <!-- select role-->
+        <div class="input-group flex flex-col space-y-5 w-full">
+            <select class="select select-success w-full max-w-xs"  name="role" >
+                <option  disabled selected class="text-xl text-black " >Pick your role</option>
+                <option class="text-xl text-black " value="manager">manager</option>
+                <option class="text-xl text-black " value="doctor">doctor</option>
+                <option class="text-xl text-black " value="sick">sick</option>
+            </select>
         </div>
 
-        <!--Checkbox-->
-        <div class="mb-6 block min-h-[1.5rem] pl-[1.5rem]">
-            <input
-                class="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-                type="checkbox"
-                value=""
-                id="checkboxDefault" />
-            <label
-                class="inline-block pl-[0.15rem] hover:cursor-pointer"
-                for="checkboxDefault">
-                Check me out
-            </label>
+        <!-- links -->
+        <div class="btn-group flex justify-between items-center">
+            <button type="submit" class="bg-green-700 text-white py-2 px-6 rounded-full text-xl font-normal hover:bg-green-600 cursor-pointer">Sign Up</button>
+            <div class="text-red-700 text-lg hover:text-red-500 cursor-pointer" id="login"><a href="login">already have an account? login here</a></div>
         </div>
-
-        <!--Submit button-->
-        <button
-            type="submit"
-            class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-            data-te-ripple-init
-            data-te-ripple-color="light">
-            Submit
-        </button>
-    </form>
-</div>
+        <!-- social media icons -->
+        <div class="border 2xl:border-solid border-gray-500"></div>
+        <div class="flex justify-between">
+            <div class="bg-wood border border-gray-600 rounded-xl px-5 py-1 hover:bg-wood3">
+                <iconify-icon width="36" height="36" icon="devicon:facebook"></iconify-icon>
+            </div>
+            <div class="bg-wood border border-gray-600 rounded-xl px-5 py-1 hover:bg-wood3">
+                <iconify-icon width="36" height="36" icon="flat-color-icons:google"></iconify-icon>
+            </div>
+            <div class="bg-wood border border-gray-600 rounded-xl px-5 py-1 hover:bg-wood3">
+                <iconify-icon width="36" height="36" icon="basil:apple-solid"></iconify-icon>
+            </div>
+            <div class="bg-wood border border-gray-600 rounded-xl px-5 py-1 hover:bg-wood3">
+                <iconify-icon width="36" height="36" icon="logos:whatsapp-icon"></iconify-icon>
+            </div>
+        </div>
+    </div>
+</form>
+<script>
+    tailwind.config = {
+        theme: {
+            extend: {
+                colors: {
+                    wood: '#EDD59E',
+                    subtle_gray:'#EDF1FF',
+                    wood2:'#ebd193',
+                    wood3:'#e7c87e',
+                }
+            }
+        }
+    }
+</script>
+<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+<!--      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>-->
+<script src="../node_modules/jquery/dist/jquery.min.js"></script>
+<script src="../assets/js/login.js"></script>
+<script src='../assets/js/forms_jquery.js'>
+</script>
