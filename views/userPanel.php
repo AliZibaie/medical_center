83ea2d9522f18e1216bf0d86d06838420c4d3273
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-28 14:57:34 +0330

    feat: add user panel

---
+++ views/userPanel.php
@@ -0,0 +1,20 @@
+<!--<button type="button" class="btn">Profile</button>-->
+<div class="drawer drawer-end">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+        <!-- Page content here -->
+        <label for="my-drawer-4" class="drawer-button btn btn-primary">Profile</label>
+    </div>
+    <div class="drawer-side">
+        <label for="my-drawer-4" class="drawer-overlay"></label>
+        <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            <!-- Sidebar content here -->
+
+            <li>
+                <form action="" method="post">
+                    <button type="submit" class="text-red-900" name="logout">Logout</button>
+                </form>
+            </li>
+        </ul>
+    </div>
+</div>
