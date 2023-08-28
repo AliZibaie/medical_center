2e979a402f4291d65c3556f25d8c523ef9cafd70
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-28 22:45:54 +0330

    feat: add new background color

--- views/layout/auth.php
+++ views/layout/auth.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html>
+<html data-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./dist/output.css" rel="stylesheet">
     <title>{{title}}</title>
-<!--    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.6.2/dist/full.css" rel="stylesheet" type="text/css" />-->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.6.2/dist/full.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
