ec42038c2c1b117515b429f76a5dde8f7c7d0d8b
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-28 14:56:48 +0330

    feat: complete LoginController class

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,8 +1,73 @@
 <?php
 
-namespace controllers;
+namespace app\app\controllers;
 
-class LoginController
+use app\app\core\Application;
+use app\app\core\SiteController;
+use app\app\helper\Session;
+
+class LoginController extends SiteController
 {
+    public function show()
+    {
+        if (Application::getApp()->getRequest()->isPost()){
+            if ($_POST['role'] == 'doctor'){
 
+                $params = $this->getDB()->table('doctor')->select()->fetchAll();
+                return $this->extracted($params);
+
+            }
+            if ($_POST['role'] == 'manager'){
+
+                $params = $this->getDB()->table('manager')->select()->fetchAll();
+                return $this->extracted($params);
+
+            }
+            if ($_POST['role'] == 'sick'){
+
+                $params = $this->getDB()->table('patient')->select()->fetchAll();
+                foreach ($params as $param){
+                    if ($_POST['username'] == $param->full_name){
+                        Session::setSession(new Session($_POST['username'], $_POST['role']));
+                        return $this->render('home', $params);
+                    }
+                }
+                $this->setLayout('auth');
+                return $this->render('signup');
+
+            }
+            return $this->render('/');
+        }
+
+        $this->setLayout('auth');
+        return $this->render('login');
+    }
+
+    /**
+     * @param $params
+     * @return string
+     */
+    public function extracted($params): string
+    {
+        foreach ($params as $param) {
+            if ($_POST['username'] == $param->full_name) {
+                foreach ($params as $param2) {
+                    if ($param->confirmation_status == '0') {
+                        $this->setLayout('auth');
+                        return $this->render('signup');
+                    }
+                }
+                Session::setSession(new Session($_POST['username'], $_POST['role']));
+                return $this->render('home', $params);
+            }
+        }
+        $this->setLayout('auth');
+        return $this->render('signup');
+    }
+
+    public function logout()
+    {
+        Session::destroy();
+        $this->render('register');
+    }
 }
