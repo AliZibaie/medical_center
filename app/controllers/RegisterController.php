9f6cbbfb2a3f49ca807dd4a4b862605dcde0e11e
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-28 15:02:21 +0330

    feat: add database login check method

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -1,8 +1,27 @@
 <?php
 
-namespace controllers;
+namespace app\app\controllers;
 
-class RegisterController
+use app\app\core\Application;
+use app\app\core\Request;
+use app\app\core\SiteController;
+
+class RegisterController extends SiteController
 {
+    public function __construct()
+    {
+        $this->setLayout('auth');
+    }
+
+
+    public function show()
+    {
+        if (Application::getApp()->getRequest()->isPost()){
+            return $this->render('login');
+        }
+
+        $this->setLayout('auth');
+        return $this->render('signup');
+    }
 
 }
