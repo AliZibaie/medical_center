d36c375e351c0875b74924cd44f2ad8418110950
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-30 01:42:08 +0330

    feat: apply flex center

--- views/404.php
+++ views/404.php
@@ -1,6 +1,6 @@
-<section class="bg-white dark:bg-gray-700 ">
-    <div class="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
-        <div class="wf-ull lg:w-1/2">
+<section class=" ">
+    <div class="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12 flex flex-col justify-center items-center">
+        <div class="w-full lg:w-1/2 flex flex-col justify-center items-center">
             <p class="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
             <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
             <p class="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn't exist.Here are some helpful links:</p>
