35f5ab64a56ad188343612aff6a60c2fa2c35de6
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-30 01:40:31 +0330

    refactor: remove some options from manager panel

--- views/managerPanel.php
+++ views/managerPanel.php
@@ -9,18 +9,8 @@
     <div class="drawer-side">
         <label for="my-drawer-4" class="drawer-overlay"></label>
         <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-            <!-- Sidebar content here -->
             <li>
-                <a href="profile" class="  rounded-box border border-4 border-red-700 text-green-800">
-                    Edit
-                    <iconify-icon icon="grommet-icons:status-warning"></iconify-icon>
-                </a>
-                <a href="departments" class="  rounded-box border border-4 border-red-700 text-green-800">
-                    manage departments
-                </a>
-            </li>
-            <li>
-                <a href="managementStatus">
+                <a href="doctors">
                     manage status
                 </a>
             </li>
