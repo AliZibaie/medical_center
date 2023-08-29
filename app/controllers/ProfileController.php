2faf29d11e08d84efa611fa524048451b5da5089
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-29 22:13:31 +0330

    feat: complete doctor profile

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -8,23 +8,26 @@ use app\app\core\SiteController;
 class ProfileController extends SiteController
 {
 
+
+
+
+    public function getDoctorInfo() : array
+    {
+        return  $this->getDB()->table('doctor')->select()->where(' full_name ','\''.$_SESSION['name'].'\'')->fetchAll();
+    }
+    public function getDepartmentInfo()
+    {
+        return  $this->getDB()->table('department')->select()->fetchAll();
+    }
+
+    public function getDoctorDepartment()
+    {
+
+    }
+
     public function show()
     {
         if (Application::getApp()->getRequest()->isPost()){
-            /*
-            $path = $_FILES['profile']['tmp_name'];
-            $name = rand(1000,100000).$_FILES['profile']['name'];
-          $username = $_SESSION['name'];
-            $role = $_SESSION['role'];
-
-        if (!is_dir("data/$username")){
-            mkdir("data/$username");
-        }
-        move_uploaded_file($path, "data/$username");
-
-            $params = Application::getApp()->getDatabase()->table('doctor')->select()->where('full_name', $username)->fetchAll();
-            */
-            $day = [];
             $saturday = $_POST['saturday'];
             $sunday = $_POST['sunday'];
             $monday = $_POST['monday'];
@@ -32,24 +35,74 @@ class ProfileController extends SiteController
             $wednesday = $_POST['wednesday'];
             $thursday = $_POST['thursday'];
             $friday = $_POST['friday'];
+            $username = $_SESSION['name'];
+            $role = $_SESSION['role'];
             $experience= $_POST['experience'];
             $bio= $_POST['bio'];
+
             $day = [$saturday,$sunday,$monday,$tuesday,$wednesday,$thursday, $friday ];
             $day =  array_filter($day);
             $day = implode(',', $day);
-            $username = $_SESSION['name'];
-            $role = $_SESSION['role'];
             Application::getApp()->getDatabase()->table($role)->update(['educational_info'=>$bio])->where(' full_name ','\''.$username.'\'')->exec();
-            Application::getApp()->getDatabase()->table($role)->update(['work_day'=>$day])->where(' full_name ','\''.$username.'\'')->exec();
+            if (!empty($day)){
+                Application::getApp()->getDatabase()->table($role)->update(['work_day'=>$day])->where(' full_name ','\''.$username.'\'')->exec();
+            }
             Application::getApp()->getDatabase()->table($role)->update(['experience'=>$experience])->where(' full_name ','\''.$username.'\'')->exec();
             if (isset($_POST['name'])){
                 Application::getApp()->getDatabase()->table($role)->update(['full_name'=>$_POST['name']])->where(' full_name ','\''.$username.'\'')->exec();
+                $_SESSION['name'] = $_POST['name'];
+            }
+            if (isset($_FILES['profile']) && $role == 'doctor'){
+                $params = $this->getDB()->table('doctor')->select()->fetchAll();
+                $profileSrc= $_FILES['profile'];
+                $profileName = rand(1000000,100000000).$profileSrc['name'];
+                $pathinfo = pathinfo($profileName);
+     
+                $tmpName = $profileSrc['tmp_name'];
+                $profileSrc = "./data/$username/$profileName";
+                if (!is_dir("./data/$username/")){
+                    mkdir("./data/$username/");
+                }
+                $id = $this->getDB()->table('doctor')->select(['id'])->where(' full_name ','\''.$username.'\'')->fetchAll();
+                if ($pathinfo['extension'] == 'jpg'){
+                    Application::getApp()->getDatabase()->table('doctors_profile')->insert([' doctor_id '=>$id[0]->id, ' source_path '=>$profileSrc])->exec();
+                    move_uploaded_file($tmpName, $profileSrc);
+                }
+                if (isset($_POST['department'])){
+                    $department_name =  $_POST['department'];
+                    $inner = $this->getDB()->innerJoin($department_name)->fetchAll();
+                    $id = $inner[0]->id;
+                        Application::getApp()->getDatabase()->table($role)->update([' department_id '=>$id])->where(' full_name ','\''.$username.'\'')->exec();
+
+                }
+//                Application::getApp()->getDatabase()->table('doctors_profile')->insert([' source_path '=>$profileSrc])->exec();
+//                Application::getApp()->getDatabase()->table('doctors_profile')->update([' doctor_id '=>$id])->where(' doctor_id ','\''.$id[0]->id.'\'')->exec();
+//                Application::getApp()->getDatabase()->table('doctors_profile')->update(['source_path'=>$profileSrc])->where(' doctor_id ','\''.$id[0]->id.'\'')->exec();
             }
         }
-
-
+        $educational_info = $this->getDoctorInfo()[0]->educational_info;
+        $department_id = $this->getDoctorInfo()[0]->department_id;
+        $experience = $this->getDoctorInfo()[0]->experience;
+        $work_day = $this->getDoctorInfo()[0]->work_day;
+        $work_day  = explode(',', $work_day);
+        $params = [ $this->getDepartmentInfo(), "edu"=>$educational_info, 'department'=>$department_id,'experience'=>$experience,'days'=>$work_day];
         $this->setLayout('auth');
-        return $this->render('profile');
+        return $this->render('profile',$params);
     }
+
+    public static function iconNeed() : bool
+    {
+        $rows = (new ProfileController)->getDoctorInfo()[0];
+        if (empty($rows->educational_info) || empty($rows->department_id) || empty($rows->experience) || empty($rows->work_day) ){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return ProfileController
+     */
+
+
 
 }
