e7e073121a9de2c9162cf307b381ec4afc665261
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-30 01:36:37 +0330

    feat: add new page for managers status 0

---
+++ app/controllers/DoctorsListController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\app\controllers;
+
+use app\app\core\Application;
+use app\app\core\SiteController;
+
+class DoctorsListController extends  SiteController
+{
+
+    public function show(): string
+    {
+
+        $params = Application::getApp()->getDatabase()->table('doctor')->where('confirmation_status', val2: '0')->select()->fetchAll();
+        $params = [$params, "job" => 'doctor'];
+        unset($_POST);
+        return $this->render('managementStatus', $params);
+    }
+}
