396cce2d9411ba5c105f2b88bc171a150e807a37
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-30 19:53:24 +0330

    feat: call post requests

--- public/index.php
+++ public/index.php
@@ -15,21 +15,17 @@ USE app\app\core\Application;
 use  app\app\controllers\RegisterController;
 
 
-
 $app = new Application(dirname(__DIR__));
-
-//$app->getRouter()->get('/','home');
 
 
 $app->getRouter()->get('/home','home');
 $app->getRouter()->get('/','home');
-$app->getRouter()->get('/departments','departments');
 
 
 $app->getRouter()->get('/managerPanel','managerPanel');
 
 $app->getRouter()->get('/managementStatus','managementStatus');
-$app->getRouter()->get('/profile',[ProfileController::class, 'show']);
+$app->getRouter()->get('/profile',[ProfileController::class,'show']);
 $app->getRouter()->post('/managementStatus',[ManagementStatusSiteController::class, 'control']);
 $app->getRouter()->get('/managementStatus',[ManagementStatusSiteController::class, 'show']);
 $app->getRouter()->get('/signup',[RegisterController::class,'show']);
@@ -39,7 +35,10 @@ $app->getRouter()->post('/login',[LoginController::class,'show']);
 $app->getRouter()->post('/home',[LoginController::class,'logout']);
 $app->getRouter()->post('/departments',[LoginController::class,'logout']);
 $app->getRouter()->get('/departments',[DepartmentsController::class,'show']);
+
 $app->getRouter()->post('/profile',[ProfileController::class,'show']);
+
+
 $app->getRouter()->get('/doctorList',[DoctorController::class,'show']);
 $app->getRouter()->get('/doctors',[DoctorsListController::class,'show']);
 $app->getRouter()->post('/doctors',[DoctorsListController::class,'control']);
@@ -47,11 +46,7 @@ $app->getRouter()->get('/managers',[ManagersListController::class,'show']);
 $app->getRouter()->post('/managers',[ManagersListController::class,'control']);
 $app->getRouter()->get('/manageDepartments',[ManageDepartmentController::class,'show']);
 $app->getRouter()->post('/manageDepartments',[ManageDepartmentController::class,'control']);
-//$app->getRouter()->post('/alertStatus',[LoginController::class,'show']);
-//$app->getRouter()->get('/signup','signup');
 
-
-//$app->getRouter()->get('/managementStatus',[ManagementStatusSiteController::class, 'list']);
 
 
 $app->run();
