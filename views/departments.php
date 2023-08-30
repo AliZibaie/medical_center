accf967a6c52459ad0615b82834ce32ea3632869
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-31 02:24:17 +0330

    feat: add new form for new data

--- views/departments.php
+++ views/departments.php
@@ -1,11 +1,13 @@
 
-<div class="overflow-x-auto w-3/4 mx-auto flex items-start">
+<div class="overflow-x-auto w-1/2 mx-auto flex items-start">
     <table class="table table-zebra">
         <!-- head -->
         <thead>
         <tr>
             <th>Item</th>
             <th>Departments</th>
+            <th></th>
+            <th>Created</th>
         </tr>
         </thead>
         <tbody>
@@ -15,6 +17,16 @@
             <tr>
                 <th><?php echo ++$key ?></th>
                 <td><?php echo $param->department_name ?></td>
+                <td>
+                    <form action="" method="post">
+                        <input type="hidden"name="departmentDoctors" value="<?php echo $param->id?>">
+                        <button type="submit" class="link link-success" >
+                            show related doctors
+
+                        </button>
+                    </form>
+                </td>
+                <td><?php echo $param->created_at ?></td>
                 <td>
                 </td>
             </tr>
