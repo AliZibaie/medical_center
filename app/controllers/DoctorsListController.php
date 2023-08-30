730f9748bf8f153339031671930d83675f87d8a3
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-30 16:44:14 +0330

    feat: add post and get requests for doctors

--- app/controllers/DoctorsListController.php
+++ app/controllers/DoctorsListController.php
@@ -11,9 +11,19 @@ class DoctorsListController extends  SiteController
     public function show(): string
     {
 
+
         $params = Application::getApp()->getDatabase()->table('doctor')->where('confirmation_status', val2: '0')->select()->fetchAll();
         $params = [$params, "job" => 'doctor'];
         unset($_POST);
         return $this->render('managementStatus', $params);
+    }
+
+    public function control()
+    {
+        if (Application::getApp()->getRequest()->isPost()){
+            $id = (int) $_POST['accept'];
+            Application::getApp()->getDatabase()->table(' doctor ')->update([' confirmation_status  '=> 1])->where(' id ','\''.$id.'\'')->exec();
+            return $this->show();
+        }
     }
 }
