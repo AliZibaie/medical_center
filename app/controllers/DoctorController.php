afad4d7e27a18fb8c108596771b9419544e11600
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-31 01:26:20 +0330

    feat: add new query for seach engine

--- app/controllers/DoctorController.php
+++ app/controllers/DoctorController.php
@@ -2,6 +2,7 @@
 
 namespace app\app\controllers;
 
+use app\app\core\Application;
 use app\app\core\SiteController;
 
 class DoctorController extends  SiteController
@@ -16,5 +17,49 @@ class DoctorController extends  SiteController
 
         $this->setLayout('main');
         return $this->render('doctorList',$this->getDoctors());
+    }
+
+    public function control()
+    {
+        if (Application::getApp()->getRequest()->isPost()){
+
+            $search = $_POST['search'];
+            $filter = $_POST['filter'];
+            if ($filter == 'desc'){
+                if (isset($search)){
+                    $query = "SELECT * FROM `doctor` WHERE full_name LIKE '%$search%'  AND confirmation_status = 1 ORDER BY full_name DESC ";
+                }else{
+                    $query = "SELECT * FROM `doctor` WHERE confirmation_status = 1 ORDER BY full_name DESC ";
+                }
+
+                $this->getDB()->setSql($query);
+                $info = $this->getDB()->fetchAll();
+                return $this->render('doctorList',$info);
+            }
+            if ($filter == 'asc'){
+                if (isset($search)){
+                    $query = "SELECT * FROM doctor WHERE full_name LIKE '%$search%' AND confirmation_status = 1 ORDER BY  full_name ASC ";
+
+            }else{
+                $query = "SELECT * FROM `doctor` WHERE confirmation_status = 1 ORDER BY full_name DESC ";
+            }
+
+                $this->getDB()->setSql($query);
+                $info = $this->getDB()->fetchAll();
+
+                 return $this->render('doctorList',$info);
+
+            }
+            if (isset($search)){
+                $query = "SELECT * FROM doctor WHERE full_name LIKE '%$search%' AND  confirmation_status = 1 ";
+                $this->getDB()->setSql($query);
+                $info = $this->getDB()->fetchAll();
+                return $this->render('doctorList',$info);
+            }
+            $query = "SELECT * FROM doctor WHERE  confirmation_status = 1 ";
+            $this->getDB()->setSql($query);
+            $info = $this->getDB()->fetchAll();
+            return $this->render('doctorList',$info);
+        }
     }
 }
