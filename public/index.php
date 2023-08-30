17cb2335220df7b08c49d2009acef00ec72dd50e
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-30 16:45:46 +0330

    feat: add get and post requests for lists

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 session_start();
 require_once __DIR__ . "/../vendor/autoload.php";
 
+use app\app\controllers\DepartmentsController;
 use app\app\controllers\DoctorController;
 use app\app\controllers\DoctorsListController;
 use app\app\controllers\LoginController;
@@ -37,10 +38,13 @@ $app->getRouter()->get('/login',[LoginController::class,'show']);
 $app->getRouter()->post('/login',[LoginController::class,'show']);
 $app->getRouter()->post('/home',[LoginController::class,'logout']);
 $app->getRouter()->post('/departments',[LoginController::class,'logout']);
+$app->getRouter()->get('/departments',[DepartmentsController::class,'show']);
 $app->getRouter()->post('/profile',[ProfileController::class,'show']);
 $app->getRouter()->get('/doctorList',[DoctorController::class,'show']);
 $app->getRouter()->get('/doctors',[DoctorsListController::class,'show']);
+$app->getRouter()->post('/doctors',[DoctorsListController::class,'control']);
 $app->getRouter()->get('/managers',[ManagersListController::class,'show']);
+$app->getRouter()->post('/managers',[ManagersListController::class,'control']);
 $app->getRouter()->get('/manageDepartments',[ManageDepartmentController::class,'show']);
 $app->getRouter()->post('/manageDepartments',[ManageDepartmentController::class,'control']);
 //$app->getRouter()->post('/alertStatus',[LoginController::class,'show']);
