0112cb8c97acc13818298472b6a7ebf37927797a
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-08-30 16:43:50 +0330

    feat: add post and get requests

--- app/controllers/managersListController.php
+++ app/controllers/managersListController.php
@@ -10,9 +10,20 @@ class managersListController extends  SiteController
     public function show(): string
     {
 
+
         $params = Application::getApp()->getDatabase()->table('manager')->where('confirmation_status', val2: '0')->select()->fetchAll();
         $params = [$params, "job" => 'manager'];
         unset($_POST);
         return $this->render('managementStatus', $params);
+    }
+
+    public function control()
+    {
+        if (Application::getApp()->getRequest()->isPost()){
+            $id = (int) $_POST['accept'];
+
+            Application::getApp()->getDatabase()->table(' manager ')->update([' confirmation_status  '=> 1])->where(' id ','\''.$id.'\'')->exec();
+            return $this->show();
+        }
     }
 }
