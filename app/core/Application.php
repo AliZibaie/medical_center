4e7af5b568592a7d3d5887d3d71a42e580db6318
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-09-02 21:37:11 +0330

    feat: add getter  for Contoller

--- app/core/Application.php
+++ app/core/Application.php
@@ -37,7 +37,7 @@ class Application
         self::$app  = $this;
         $this->response = new Response();
         $this->controller = new SiteController();
-        $this->router = new Router($this->request, $this->response);
+        $this->router = new Router($this->request,$this->response);
         $this->database = new MySqlDatabase(MySqlDatabaseConnection::getInstance());
     }
     public function run()
