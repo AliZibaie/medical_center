93f16472e2a6de8f4c591b77dff474ffcaecf5eb
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-09-02 21:33:51 +0330

    feat: add new filter and sort in search box

--- views/doctorList.php
+++ views/doctorList.php
@@ -9,6 +9,7 @@
                 <th>Experience</th>
                 <th class="" style="width: 200px;">Day</th>
                 <th class="" style="width: 200px;">Rating</th>
+                <th class="" style="width: 200px;">Profile</th>
             </tr>
             </thead>
             <tbody class="">
@@ -30,14 +31,23 @@
                     <th> <?php echo  $param->work_day ?></th>
                     <th>
                         <div class="rating">
-                            <input type="radio" name="rating-<?php echo $key ?>" class="mask mask-star-2  bg-green-500" />
+                            <button type="submit">
+                                <input type="radio" name="rating-<?php echo $key ?>" class="mask mask-star-2  bg-green-500" />
+                            </button>
                             <input type="radio" name="rating-<?php echo $key ?>" class="mask mask-star-2  bg-green-500"/>
                             <input type="radio" name="rating-<?php echo $key ?>" class="mask mask-star-2  bg-green-500" checked  />
                             <input type="radio" name="rating-<?php echo $key ?>" class="mask mask-star-2  bg-green-500" />
                             <input type="radio" name="rating-<?php echo $key ?>" class="mask mask-star-2  bg-green-500" />
                         </div>
                     </th>
-
+                    <th>
+                        <form action="" method="post">
+                            <input type="hidden" name="doctors-profile" value="<?php echo  $param->id?>">
+                            <button type="submit" class="link link-success" >
+                                <iconify-icon width="26" height="26" icon="pajamas:profile"></iconify-icon>
+                            </button>
+                        </form>
+                    </th>
                 </tr>
             <?php }?>
             </tbody>
@@ -47,18 +57,48 @@
     <div class="join w-1/4">
         <div>
             <div>
-                <input class="input input-bordered join-item" placeholder="Search" name="search">
+                <input class="input input-bordered join-item" placeholder="Search" name="search" id="live_search">
             </div>
         </div>
-        <select class="select select-bordered join-item w-24" name="filter">
+        <select class="select select-bordered join-item w-24" name="filterby">
             <option disabled selected>Filter</option>
+            <option value="asc">Expertise</option>
+            <option value="desc">days</option>
+            <option value="all">name</option>/
+        </select>
+        <select class="select select-bordered join-item w-24" name="sortby">
+            <option disabled selected>Sort</option>
             <option value="asc">Ascending</option>
             <option value="desc">Descending</option>
-            <option value="all">All</option>
+<!--            <option value="all">All</option>-->
         </select>
         <div class="indicator">
             <button class="btn btn-accent btn-outline join-item" type="submit">Search</button>
         </div>
     </div>
     </form>
+<!--    <div id="bolbol"></div>-->
 </div>
+
+<!--<script>-->
+<!--    $(document).ready(function (){-->
+<!--        $('#live_search').keyup(function (){-->
+<!--            var input  = $(this).val();-->
+<!---->
+<!--            if (input !== ''){-->
+<!--                $.ajax({-->
+<!--                    url:"",-->
+<!--                    method :"POST",-->
+<!--                    data: {input:input},-->
+<!--                    success:function (data){-->
+<!--                        // $('#bolbol').html(data);-->
+<!--                        //var simple = ' //echo $params; ////';-->
+<!--//                        var simple = input;-->
+<!--//                        $('#bolbol').text(simple);-->
+<!--//                    }-->
+<!--//                })-->
+<!--//            }-->
+<!--//        })-->
+<!--//    })-->
+<!--//-->
+<!--//</script>-->
